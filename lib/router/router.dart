a588030372c54e71adbbada4f72951b9dc220dea
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-07-11 20:15:09 +0900

    refactor

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:poke_poke_dex/api/client/client.dart';
+import 'package:poke_poke_dex/api/client/pokeapi_client.dart';
 
 import 'package:poke_poke_dex/ui/screen/detail_screen.dart';
 import 'package:poke_poke_dex/ui/screen/list_screen.dart';
