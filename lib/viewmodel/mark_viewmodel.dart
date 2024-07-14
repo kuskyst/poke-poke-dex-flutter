946c1e1a4e8b36ecba4fd4e3b0acb3f32fa55264
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-07-14 22:07:21 +0900

    Update mark_viewmodel.dart

--- lib/viewmodel/mark_viewmodel.dart
+++ lib/viewmodel/mark_viewmodel.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:poke_poke_dex/isar/client/isar_client.dart';
@@ -12,19 +12,26 @@ class MarkViewmodel extends ChangeNotifier {
   get mark => _mark;
 
   Future<void> fetchMarks(String inId) async {
-    final isar = Isar.getInstance()?.isOpen ?? false
-      ? Isar.getInstance() : await isarOpen();
-    _mark = (await isar!.marks.filter().inIdMatches(inId).findFirst());
-    _mark ??= Mark(inId: inId);
+    if (!kIsWeb) {
+      final isar = Isar.getInstance()?.isOpen ?? false
+        ? Isar.getInstance() : await isarOpen();
+      _mark = (await isar!.marks.filter().inIdMatches(inId).findFirst());
+      _mark ??= Mark(inId: inId);
+    } else {
+      _mark = Mark(inId: inId);
+    }
+    
   }
 
   Future<void> putMark(Mark mark) async {
-    final isar = Isar.getInstance()?.isOpen ?? false
-      ? Isar.getInstance() : await isarOpen();
-    await isar!.writeTxn(() async {
-      await isar.marks.put(mark);
-    });
-    fetchMarks(mark.inId);
+    if (!kIsWeb) {
+      final isar = Isar.getInstance()?.isOpen ?? false
+        ? Isar.getInstance() : await isarOpen();
+      await isar!.writeTxn(() async {
+        await isar.marks.put(mark);
+      });
+    }
+      fetchMarks(mark.inId);
   }
 
 }
